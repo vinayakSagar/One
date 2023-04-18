0a6ad4b70a6e0ec6cd481c22ebc20926e69cbc1f
vinayakSagar <vinayaksagar444@gmail.com>
2023-04-18 00:50:08 -0700

    Added assests font created navbar Header component and Home component Routing is done for Home component only.

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+    return (
+        <>
+            <nav>
+                <h1>TechStar.</h1>
+                <main>
+                    <Link to={"/"}>Home</Link>
+                    <Link to={"/contact"}>Contatc</Link>
+                    <Link to={"/#about"}>About</Link>
+                    <Link to={"/#brands"}>Brands</Link>
+                    <Link to={"/services"}>Services</Link>
+                </main>
+            </nav>
+        </>
+    )
+}
+
+export default Header
