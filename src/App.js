a0c386999ad5a254bd1d19806a47742426ed57c5
vinayakSagar <vinayaksagar444@gmail.com>
2023-05-21 16:51:41 +0530

    home3 div started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 import "./styles/App.scss"
 import "./styles/header.scss"
-import Home from "./components/Home";
+import "./styles/home.scss"
 
 function App() {
   return (
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import vg from '../assets/2.webp';
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <div className="home">
+        <main>
+          <h1>TechStar</h1>
+          <p>Solution to all your problems.</p>
+        </main>
+      </div>
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+        <div>
+          <p>We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.</p>
+        </div>
+      </div>
+      <div className="home3">
+        <div>
+          <h1>Who we are?</h1>
+        </div>
+      </div>
+    </>
   )
 }
 
