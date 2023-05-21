ecae997f45dd1b792e8f5c51d1ea272d782436e1
vinayakSagar <vinayaksagar444@gmail.com>
2023-05-21 17:17:37 +0530

    home4 div started

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,6 +21,22 @@ function Home() {
       <div className="home3">
         <div>
           <h1>Who we are?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            officiis hic illo quos sit commodi, aut magnam odio eligendi? Totam
+            quis officiis fugit sunt dolores nostrum tenetur iusto est odio
+            mollitia. Dolor placeat repellendus officia aspernatur dolorum
+            harum. Dolorem cumque quaerat eius qui deserunt ullam laudantium
+            quia tempore. Dignissimos esse sequi numquam est ipsam nemo,
+            voluptas vel recusandae iste aspernatur qui quasi, commodi, atque
+            ipsa dolores tempore asperiores? Dolores soluta placeat molestiae
+            aperiam, ullam officia fugiat corrupti odio, suscipit ipsum cum vel
+            nemo. Ratione quo tempore fugit quaerat! Facilis beatae impedit
+            similique maxime praesentium sunt unde necessitatibus voluptates
+            ipsa, et, numquam reiciendis eveniet? Aliquid nemo nesciunt placeat,
+            qui fuga quae repellat sunt, non natus, iure ab officia rem ipsa cum
+            sed cumque corporis voluptate quibusdam nostrum quod saepe
+          </p>
         </div>
       </div>
     </>
